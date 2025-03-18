9cfd0636dd459c363a264cf4324dbb8878e90958
chukwukelu2023 <chukwukeluoo@gmail.com>
2025-03-18 14:04:47 +0100

    added external db

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,10 +1,30 @@
-import { Sequelize } from "sequelize";
+import { Dialect, Sequelize } from "sequelize";
+import dotenv from "dotenv"
+dotenv.config()
 
-export const sequelize = new Sequelize('taskmanager','','',{
-    dialect: 'sqlite',
-    storage: './database.sqlite',
-    logging:false
+const database = process.env.DATABASE as string
+const username = process.env.DATABASE_USERNAME as string
+const password = process.env.DB_PASSWORD as string
+const host = process.env.HOST as string
+const port = process.env.DB_PORT as string
+const dialect = process.env.DIALECT as Dialect
+
+
+  export const sequelize = new Sequelize({
+    database: database,
+    dialect: dialect,
+    username: username,
+    password: password,
+    port: +port,
+    logging:false,
+    host:host,
   });
+
+// export const sequelize = new Sequelize('taskmanager','','',{
+//     dialect: 'sqlite',
+//     storage: './database.sqlite',
+//     logging:false
+//   });
 
 export async function dbConnection(){
     try {
